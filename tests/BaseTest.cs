89de8931a33bafdba8f0d6f8ad6c0576770831f3
MARCELO GOMES RIBEIRO <marcelo.ribeiro@kroton.com.br>
2020-03-29 15:51:27 -0300

    removido credencial

--- tests/BaseTest.cs
+++ tests/BaseTest.cs
@@ -6,11 +6,11 @@ namespace B2Net.Tests {
 		public B2Options Options { get; set; }
 
 		//TODO: Alterar credenciais
-		protected string applicationKey = "00022d3d9f6579050f1fea8606aca29ca9e324a689";
-		protected string applicationKeyId = "16ba0ce97c1d";
+		protected string applicationKey = "";
+		protected string applicationKeyId = "";
 
-		protected string restrictedApplicationKey = "K0001Uev/N/Sd8dvVuP0vojW0D57wgc";
-		protected string restrictedApplicationKeyId = "00016ba0ce97c1d0000000003";
+		protected string restrictedApplicationKey = "";
+		protected string restrictedApplicationKeyId = "";
 
 		public BaseTest() {
 			Options = new B2Options() {
--- tests/TestConstants.cs
+++ tests/TestConstants.cs
@@ -7,8 +7,8 @@ using System.Threading.Tasks;
 namespace B2.Net.Tests {
 	public static class TestConstants {
 		//TODO: Alterar credenciais
-		public static string ApplicationKey = "00022d3d9f6579050f1fea8606aca29ca9e324a689";
+		public static string ApplicationKey = "";
 
-		public static string KeyId = "16ba0ce97c1d";
+		public static string KeyId = "";
 	}
 }
